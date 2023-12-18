f005bea964e3ca1e5113742a125528c8a059c273
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-12-18 14:03:14 -0800

    made function for the pint opcode, and added prototype to header file

--- monty.h
+++ monty.h
@@ -41,5 +41,6 @@ void execute_op(char *opcode, stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t *stack);
+void pint(stack_t **stack, unsigned int line_number);
 
 #endif
