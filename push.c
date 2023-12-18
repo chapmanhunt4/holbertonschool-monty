06964bf932649c71ea821580c0d271827555f11b
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-12-18 12:55:08 -0800

    made check for prev pointer to point to new node if stack is not empty

--- push.c
+++ push.c
@@ -26,5 +26,7 @@ void push(stack_t **stack, unsigned int line_number)
 	new_node->n = num;
 	new_node->prev = NULL;
 	new_node->next = *stack;
+	if (*stack != NULL)
+		(*stack)->prev = new_node;
 	*stack = new_node;
 }
