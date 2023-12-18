23556e63fb08fc1fc2a86edd9bbb440680e79379
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-12-18 14:37:47 -0800

    handled data assignment when swapping

--- swap.c
+++ swap.c
@@ -10,12 +10,17 @@ void swap(stack_t **stack, unsigned int line_number)
 {
 	stack_t *first = *stack;
 	stack_t *second = (*stack)->next;
+	int temp;
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
+
+	temp = first->n;
+	first->n = second->n;
+	second->n = temp;
 
 	first->next = second->next;
 	second->next = first;
